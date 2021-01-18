817e5fc9e81266dae91cf130753f7aca7c6b9cc0
Nicklas Møller Jepsen <nicklas.m.jepsen@gmail.com>
2021-01-18 12:41:50 +0100

    added required nugets

---
+++ AzureFunctionsSample.DependencyInjection/Startup.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.Azure.Functions.Extensions.DependencyInjection;
+using Microsoft.Extensions.DependencyInjection;
+
+[assembly: FunctionsStartup(typeof(AzureFunctionsSample.DependencyInjection.Startup))]
+
+namespace AzureFunctionsSample.DependencyInjection
+{
+    public class Startup : FunctionsStartup
+    {
+        public override void Configure(IFunctionsHostBuilder builder)
+        {
+            builder.Services.AddHttpClient();
+
+            //builder.Services.AddSingleton<IMyService>((s) => {
+            //    return new MyService();
+            //});
+
+            //builder.Services.AddSingleton<ILoggerProvider, MyLoggerProvider>();
+        }
+    }
+}
